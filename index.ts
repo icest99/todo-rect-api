14cd867c768b5f80af9f337ef2475010d5079f3f
Kiattipan Muangyai <playicest@gmail.com>
2023-02-07 00:40:01 +0700

    change port

--- index.ts
+++ index.ts
@@ -20,7 +20,7 @@ const port = process.env.PORT;
 export const AppDataSource = new DataSource({
     type: 'mysql',
     host: 'ap-southeast.connect.psdb.cloud',
-    port: 10000,
+    port: 3306,
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DB,
